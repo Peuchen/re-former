3fa4c8e04ed3cc56b9526ea6821abb607e4a96a2
Peuchen <npeuchen@gmail.com>
2022-04-15 16:45:23 +0200

    Add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,22 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   private
